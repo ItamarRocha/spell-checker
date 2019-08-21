734c7cc2c61a2fe8a201834f14216ce5d639f891
void-zero <wallace.lucena@hotmail.com>
2019-08-21 00:13:37 -0300

    First changes to main

--- main.c
+++ main.c
@@ -1,9 +1,83 @@
-#include "stdio.h"
-#include "stdlib.h"
+#include <stdio.h>
+#include <string.h>
 
-int main(){
+#define MAX_LENGHT 40
+#define NUM_BUCKETS 100 //Arrumar
+#define DICTIONARY_DIR "*.txt"
 
-	printf("Wallace eh homem");
+typedef struct node{
+    char value[MAX_LENGHT];
+    struct node *next;
+}tNode;
 
-	return 0;
+typedef struct TableElements{
+    int lengthOfElements;
+    tNode* node;
+}tTableElements;
+
+typedef struct HashTable{
+    int NumBuckets;
+    tTableElements* buckets;
+}tHashTable;
+
+tHashTable* newHashTable(){
+    int i;
+    tHashTable* t1 = (tHashTable*) malloc(sizeof(tHashTable));
+    t1->NumBuckets = NUM_BUCKETS;
+    for(i = 0; i < NUM_BUCKETS; i++)
+        t1->buckets[i] = NULL;
+    return t1;
+}
+
+
+tNode *buckets[NUM_BUCKETS];
+
+unsigned int h(char* key){
+    //Funcao hash
+}
+
+void readDictionary(){
+
+}
+
+int check(char** argv, tHashTable* t1){
+    FILE *fp = fopen(argv[1], "r");
+    tNode* cursor;
+    char *string, found;
+    int errorSum = 0;
+    
+    while(fscanf(fp, "%s", string) != EOF){
+        cursor = buckets[h(string)];   //Sujeito a alteracoes, de acordo com a implementacao dos buckets.
+        found = 0;
+        
+        while(cursor != NULL){    //Ou seja, caso chegarmos ao fim do bucket, paramos.
+            if(!strcmp(cursor->data, string)){
+                found = 1;
+                break;
+            }
+            cursor = cursor->next;
+        }
+        
+        if(!found)
+            errorSum++;
+    }
+    
+    fclose(fp);
+    return errorSum;
+}
+
+int main(int argc, char** argv){
+    if (argc < 2) {
+        printf("\nFaltando parametros\n");
+        printf(" ./exec [Arquivo.txt] \n");
+        return 1;
+    }
+
+    if (argc > 2) {
+        printf("\nMuitos parametros\n");
+        printf(" ./exec [Arquivo.txt] \n");
+        return 1;
+    }
+    
+    return 0;
 }
