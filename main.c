bc9d32e1cf49ae4a454a981891337301be6d5406
ItamarRocha <itamardprf@gmail.com>
2019-08-21 14:08:06 -0300

    main with problems of assigning

--- main.c
+++ main.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 #define MAX_LENGHT 40
 #define NUM_BUCKETS 100 //Arrumar
 #define DICTIONARY_DIR "*.txt"
 
 typedef struct node{
-    char value[MAX_LENGHT];
+    char *value;
     struct node *next;
 }tNode;
 
@@ -21,15 +22,32 @@ typedef struct HashTable{
 
 tHashTable* newHashTable(){
     int i;
+    char* string;
+    sprintf(string , "oi wallace");
+    printf("%s\n",string);
     tHashTable* t1 = (tHashTable*) malloc(sizeof(tHashTable));
     t1->NumBuckets = NUM_BUCKETS;
-    for(i = 0; i < NUM_BUCKETS; i++)
-        t1->buckets[i]->node = NULL;
+    t1->buckets = (tBuckets *) malloc(sizeof(tBuckets));
+    
+    for(i = 0; i < NUM_BUCKETS; i++){
+        t1->buckets[i].node = (tNode*) malloc(sizeof(tNode));
+        t1->buckets[i].node->next = NULL;
+        //printf("to passando aqui %d\n",i);
+        t1->buckets[10].node->value = string;
+    }
+        
+        
+        
     return t1;
 }
 
 unsigned int h(char* key){
-    //Funcao hash
+    unsigned int result = 0;
+    int i = 0;
+    for(i = 0 ; key[i] != '\0'; i++){
+        result += key[i]*(i + 1);
+    }
+    return result%100;
 }
 
 void readDictionary(){
@@ -38,13 +56,14 @@ void readDictionary(){
 
 int check(char** argv, tHashTable* t1){
     FILE *fp = fopen(argv[1], "r");
-    tHashTable* Hasht1 = newHashTable();
     tNode* cursor;
     char *string, found;
     int errorSum = 0;
     
     while(fscanf(fp, "%s", string) != EOF){
-        cursor = Hasht1->buckets[h(k)]->node;   //Sujeito a alteracoes, de acordo com a implementacao dos buckets.
+        printf("%s\n",string);
+        printf("%ud\n", h(string));
+        cursor = t1->buckets[h(string)].node;   //Sujeito a alteracoes, de acordo com a implementacao dos buckets.
         found = 0;
         
         while(cursor != NULL){    //Ou seja, caso chegarmos ao fim do bucket, paramos.
@@ -75,6 +94,12 @@ int main(int argc, char** argv){
         printf(" ./exec [Arquivo.txt] \n");
         return 1;
     }
-    
+    tHashTable* t1 = newHashTable();
+    printf("opa\n");
+    scanf("%s",t1->buckets[10].node->value);
+    //check(argv ,t1);
+    printf("opa 2\n");
+    printf("%s\n", t1->buckets[10].node->value);
+    printf("opa 3\n");
     return 0;
 }
