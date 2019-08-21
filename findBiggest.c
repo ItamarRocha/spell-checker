371fafd414e99fee00cb0378d0702a08b2e67c91
void-zero <wallace.lucena@hotmail.com>
2019-08-20 22:43:48 -0300

    A side program used to find the biggest word in the dictionary

---
+++ findBiggest.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char** argv){
+    char str[50], bStr[50];
+    int bIndex = 1;
+    
+    if (argc < 2) {
+        printf("\nFaltando parametros\n");
+        printf(" ./exec [Dicionario] \n");
+        return 1;
+    }
+
+    if (argc > 2) {
+        printf("\nMuitos parametros\n");
+        printf(" ./exec [Dicionario] \n");
+        return 1;
+    }
+    
+    char *file;
+    file = argv[1];
+
+    FILE *fp = fopen(file, "r");
+    
+    if(fp != NULL){
+        for(int i = 1; i <= 307855; i++){
+            fgets(str, 50, fp);
+            str[strlen(str)-1] = '\0';
+            
+            if(i==1)
+                strcpy(bStr, str);
+            
+            if(strlen(str) > strlen(bStr)){
+                bIndex = i;
+                strcpy(bStr, str);
+            }
+        } 
+        
+        fclose(fp);
+        
+        printf("Biggest: %s\nSize: %ld\nIndex : %d\n", bStr, strlen(bStr), bIndex);
+
+    }else{
+        printf("Erro ao abrir  o arquivo para leitura\n");
+        return -1;
+    }
+    
+    return 0;
+}
